dec4afc6be4889cf1e8cf98027e60ea4cdeb6a69
Ilyas Davletberdin <120725943+smartexcode@users.noreply.github.com>
2023-07-13 02:33:17 +0500

    new

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,15 @@ public class Main {
     public static void main(String[] args) {
         int a = 1;
         int b = 2;
+        int c = 2;
+        int f = 2;
+        int s = 2;
+
     }
+    String a = "a";
+    String b = "b";
+    String c = "c";
+
+    String change = "My change for Stash!";
+
 }
