367e993e049454242f0aa2136dae7b6adda22b62
Ilyas Davletberdin <120725943+smartexcode@users.noreply.github.com>
2023-07-13 22:11:12 +0500

    add 3

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         int a = 1;
-        int b = 2;
-        int c = 2;
+        int b1 = 2;
         int f = 2;
         int s = 2;
+
+        String add = "new";
+        String add2 = "new2";
+        String add3 = "new3";
 
     }
 }
